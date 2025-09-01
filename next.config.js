8d38a97b19745abd56ed0a10a2e622e4c5c76d75
Jacob Edley <edleyjacob@gmail.com>
2025-09-01 12:48:48 -0500

    end redirect to main site

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // Redirect to normal site
-    async redirects() {
-        return [
-            {
-                source: "/",
-                destination: "https://hackillinois.org/",
-                permanent: false
-            }
-        ];
-    }
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: "/",
+    //             destination: "https://hackillinois.org/",
+    //             permanent: false
+    //         }
+    //     ];
+    // }
 };
 
 module.exports = nextConfig;
